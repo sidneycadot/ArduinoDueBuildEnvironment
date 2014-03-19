417cbc971fd08222c0616e3029c935339ee7faea
Sidney Cadot <sidney@jigsaw.nl>
2014-03-19 11:34:15 +0100

    Latest version.

---
+++ README.md
@@ -0,0 +1,28 @@
+
+GOALS
+=====
+
+(1) A working cross-compiler GCC build environment for the ARM-based Arduino Due.
+(2) A working newlib, uclib, or other small C library\
+(3) The appropriate CMSIS stuff for the ARM chip
+(4) A working FreeRTOS
+(5) Flash tool
+
+Versions
+--------
+
+Current versions (March 2014):
+
+* Binutils ...... 2.24       http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.bz2
+* GCC ........... 4.8.2      ftp://ftp.gnu.org/gnu/gcc/gcc-4.8.2/gcc-4.8.2.tar.bz2
+* GMP ........... 5.1.3      https://gmplib.org/download/gmp/gmp-5.1.3.tar.lz
+* MPFR .......... 3.1.2      http://www.mpfr.org/mpfr-current/mpfr-3.1.2.tar.xz
+* MPC ........... 1.0.2      ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.2.tar.gz
+* ISL ........... 0.12.2     ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.12.2.tar.bz2
+* CLoog ......... 0.18.1     ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.1.tar.gz
+* NewLib ........ 2.1.0      ftp://sourceware.org/pub/newlib/newlib-2.1.0.tar.gz               http://sourceware.org/newlib/
+* uClibc ........ 0.9.33     http://www.uclibc.org/downloads/uClibc-0.9.33.tar.xz              http://www.uclibc.org/
+Installing GCC
+--------------
+
+http://gcc.gnu.org/install/
--- build.sh
+++ build.sh
@@ -8,33 +8,87 @@ set -e
 # isl
 # cloog
 
+# TODO: add binutils
+
 HERE=$PWD
 
-rm -rf $HERE/gcc-4.8.2
-rm -rf $HERE/build
-rm -rf $HERE/root
+DOWNLOADS_DIR=$HERE/downloads
+ROOT_DIR=$HERE/root
+
+BINUTILS_BUILD_DIR=$HERE/build-binutils
+BINUTILS_SOURCE_DIR=$HERE/binutils-2.24
+
+GCC_BUILD_DIR=$HERE/build-gcc
+GCC_SOURCE_DIR=$HERE/gcc-4.8.2
+
+TARGET=arm-none-eabi
+
+rm -rf $BINUTILS_SOURCE_DIR $BINUTILS_BUILD_DIR
+rm -rf $GCC_SOURCE_DIR      $GCC_BUILD_DIR
+rm -rf $ROOT_DIR
+
+#############################
+
+if [ ! -d $DOWNLOADS_DIR ] ; then
+    mkdir -p $DOWNLOADS_DIR
+fi
+
+cd $DOWNLOADS_DIR
+
+if [ ! -f binutils-2.24.tar.bz2 ] ; then
+    wget http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.bz2
+fi
 
 if [ ! -f gcc-4.8.2.tar.bz2 ] ; then
     wget ftp://ftp.gnu.org/gnu/gcc/gcc-4.8.2/gcc-4.8.2.tar.bz2
 fi
 
-tar xf gcc-4.8.2.tar.bz2
+if [ ! -f gmp-5.1.3.tar.lz ] ; then
+    wget https://gmplib.org/download/gmp/gmp-5.1.3.tar.lz
+fi
 
-find $HERE/gcc-4.8.2 gcc-4.8.2 -type f -print0 | xargs -0 md5sum > $HERE/Stage-1
+if [ ! -f mpfr-3.1.2.tar.xz ] ; then
+    wget http://www.mpfr.org/mpfr-current/mpfr-3.1.2.tar.xz
+fi
 
-mkdir build
-cd build
+if [ ! -f mpc-1.0.2.tar.gz ] ; then
+    wget ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.2.tar.gz
+fi
 
-# Add --target=xxx later
+if [ ! -f isl-0.12.2.tar.bz2 ] ; then
+    wget ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.12.2.tar.bz2
+fi
 
-$HERE/gcc-4.8.2/configure --prefix=$HERE/root --program-prefix=sidney --enable-languages=c,c++
+if [ ! -f cloog-0.18.1.tar.gz ] ; then
+    wget ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.1.tar.gz
+fi
 
-find $HERE/gcc-4.8.2 gcc-4.8.2 -type f -print0 | xargs -0 md5sum > $HERE/Stage-2
+############################# Build binutils
 
-make
+cd $HERE
 
-find $HERE/gcc-4.8.2 gcc-4.8.2 -type f -print0 | xargs -0 md5sum > $HERE/Stage-3
+tar xf $DOWNLOADS_DIR/binutils-2.24.tar.bz2
+
+mkdir $BINUTILS_BUILD_DIR
+cd $BINUTILS_BUILD_DIR
+
+$BINUTILS_SOURCE_DIR/configure --prefix=$HERE/root --program-prefix=sidney- --target=$TARGET
+
+make -j8
 
 make install
 
-find $HERE/gcc-4.8.2 gcc-4.8.2 -type f -print0 | xargs -0 md5sum > $HERE/Stage-4
+############################# Build gcc
+
+cd $HERE
+
+tar xf $DOWNLOADS_DIR/gcc-4.8.2.tar.bz2
+
+mkdir $GCC_BUILD_DIR
+cd $GCC_BUILD_DIR
+
+$GCC_SOURCE_DIR/configure --prefix=$HERE/root --program-prefix=sidney- --enable-languages=c,c++ --target=$TARGET
+
+make -j8
+
+make install
