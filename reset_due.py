3f48f6012ee64fcf8fd569aea71fde0a30a1857b
Sidney Cadot <sidney@jigsaw.nl>
2014-03-23 09:57:49 +0100

    Removed sleep.

--- reset_due.py
+++ reset_due.py
@@ -2,10 +2,9 @@
 
 # Touching the Due at 1200 baud resets it.
 # TODO: investigate what is meant by "touching"
-# TODO: investigate need for sleep
 # TODO: implement a C version.
 
-import serial, time, sys
+import serial, sys
 
 if len(sys.argv) > 1:
     device = sys.argv[1]
@@ -13,5 +12,4 @@ else:
     device = "/dev/ttyACM0"
 
 ser = serial.Serial(device, baudrate = 1200)
-time.sleep(1.000)
 ser.close()
